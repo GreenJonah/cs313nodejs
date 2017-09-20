b7ed5890a8219727ecda8e726f8cdf80b98bb9b2
Jonah Green <gre11017@byui.edu>
2017-09-20 12:44:12 -0600

    added function to change div1 color

--- web/Team01.js
+++ web/Team01.js
@@ -2,3 +2,10 @@
 function click() {
 	alert("Clicked!");
 }
+
+function changeColor() {
+	var newColor = document.getElementById("newColor");
+	var div1     = document.getElementById("one");
+   
+    div1.style.backgroundColor = newColor;
+}
