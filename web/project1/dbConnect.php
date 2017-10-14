f03eda413903e265b590a7bcb9a541d940e0737c
Jonah Green <gre11017@byui.edu>
2017-10-14 13:24:28 -0600

    added some echo lines to help debug

--- web/project1/dbConnect.php
+++ web/project1/dbConnect.php
@@ -20,14 +20,14 @@ function get_db() {
 		}
 		
 		$dbopts = parse_url($dbUrl);
-		//print "<p>$dbUrl</p>\n\n";
+		print "<p>$dbUrl</p>\n\n";
 		$dbHost = $dbopts["host"];
 		$dbPort = $dbopts["port"];
 		$dbUser = $dbopts["user"];
 		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
 		// Create the PDO connection
-		//print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+		print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
