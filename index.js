8b646869c98afc772d139a5445020dcf0ef6909b
Jonah Green <gre11017@byui.edu>
2017-11-28 15:46:44 -0700

    s

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
+/*
 var pg = require("pg"); // This is the postgres database connection module.
 const connectionString = "postgres://ta_user:ta_pass@localhost:5432/shim";
-
+*/
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
